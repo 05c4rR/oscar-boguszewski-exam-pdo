b328088a79a6dfa6ecfec1bbd4dbb809445f6ac5
Oscar <159458439+05c4rR@users.noreply.github.com>
2024-08-02 11:25:54 +0200

    removed unused connect.bdd

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-try {
-    $db = new PDO('mysql:host=localhost;charset=utf8;dbname=exam_pdo','php', 'PHPcestSuper!');
-    
-} catch (PDOException $e) {
-    die ('Erreur de connexion à la bdd : ' . $e->getMessage()); 
-}
-
